fa173b8fe63a74ae796cfd65fb0d43434e8b840c
Nathanael Lu <lunathanael@gmail.com>
2024-02-23 14:19:38 -0500

    added view resume to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,24 @@ export default function Home() {
           </Link> in Python.
         </h2>
       </div>
+      
+      <div className="fixed bottom-0 left-0 m-4 animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+            <a className="inline-flex items-center leading-tight group" aria-label="View Full Resume" href="/Nathanael_Lu_Resume_2024.pdf">
+              <span>
+                  <span className="border-b border-transparent pb-px transition group-hover:border-zinc-300 motion-reduce:transition-none">
+                  View Resume
+                  </span>
+                  <span className="whitespace-nowrap">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
+                          <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd">
+                          </path>
+                      </svg>
+                  </span>
+              </span>
+            </a>
+        </h2>
+      </div>
 
       <div className="fixed bottom-0 right-0 m-4 animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
