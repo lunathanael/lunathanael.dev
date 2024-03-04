22d47ea290f7087b69e05b194ec8957fb98ebaee
Nathanael Lu <lunathanael@gmail.com>
2024-03-04 14:12:01 -0500

    inital quiz

---
+++ app/seventeen/page.tsx
@@ -0,0 +1,10 @@
+import Quiz from './quiz'; // Adjust the path as necessary
+
+export default function Home() {
+  return (
+    <div className="bg-white container mx-auto px-4">
+      <h1 className="text-2xl font-bold text-center my-4">Quiz App</h1>
+      <Quiz />
+    </div>
+  );
+}
